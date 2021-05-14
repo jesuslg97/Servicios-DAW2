2f62bdaee62d7dfb2a95900cf2ccf10dca54db78
jesus <jelopezgonzalez@gmail.com>
2021-05-14 18:43:34 +0100

    toolbar and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SonBComponent } from './son-b/son-b.component';
 import { GrandSonAComponent } from './grand-son-a/grand-son-a.component';
 import { GrandSonBComponent } from './grand-son-b/grand-son-b.component';
 import { FormsModule } from '@angular/forms';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -18,14 +20,16 @@ import { FormsModule } from '@angular/forms';
     SonAComponent,
     SonBComponent,
     GrandSonAComponent,
-    GrandSonBComponent
+    GrandSonBComponent,
+    ToolbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatToolbarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
